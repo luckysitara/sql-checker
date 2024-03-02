f0e52c56405889e15b115538cc1cad62daadb715
bughacker <udoessienprecious@gmail.com>
2024-03-02 19:20:04 +0100

    m

--- sql/setup.py
+++ sql/setup.py
@@ -4,6 +4,6 @@ setup(
     name="SQL Grading System",
     version="1.0",
     description="Grading student SQL submissions",
-    executables=[Executable("me2.py")],
+    executables=[Executable("me1.py")],
 )
 
